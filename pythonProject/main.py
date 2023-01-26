a63408641dce3f5d0acd6089a9a971568482ab45
ws-user <vlad24852@gmail.com>
2023-01-26 17:41:03 +0500

    initial commit

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,2 +1,15 @@
-print("моя первая игра")
+import pygame
 
+WIOTH = 500
+WEIGHT = 500
+FPS = 100
+
+pygame.init()
+pygame.mixer.init()
+scrin = pygame.display.set_mode((WIOTH, WEIGHT))
+pygame.display.set_caption("Первая игра")
+clock = pygame.time.Clock
+
+running = True
+while running:
+    pass
